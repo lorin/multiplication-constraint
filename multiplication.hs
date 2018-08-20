ff55044c0f462a8fba45df1e41508b54411156d9
Lorin Hochstein <lhochstein@netflix.com>
2018-08-19 20:10:41 -0700

    commit

--- multiplication.hs
+++ multiplication.hs
@@ -1,5 +1,18 @@
+import Control.Monad
+
 main = putStrLn $ show solution
 
 solution :: [Int]
-solution = [1..5]
-
+solution = do
+    let digit = [0..9]
+    a <- digit
+    b <- digit
+    c <- digit
+    d <- digit
+    guard $ a/=b
+    guard $ a/=c
+    guard $ a/=d
+    guard $ b/=c
+    guard $ b/=d
+    guard $ (1000*a+100*b+10*c+d)*d == 1000*d+100*c+10*b+a
+    [a,b,c,d]
