d6a8a075f5411ef98247f2e569566eb8f6fe2f45
Lorin Hochstein <lhochstein@netflix.com>
2018-08-21 23:00:28 -0700

    haskell update

--- README.md
+++ README.md
@@ -95,13 +95,14 @@ solution = do
     b <- digit
     c <- digit
     d <- digit
-    guard $ a/=b
-    guard $ a/=c
-    guard $ a/=d
-    guard $ b/=c
-    guard $ b/=d
+    guard $ different [a,b,c,d]
     guard $ (1000*a+100*b+10*c+d)*d == 1000*d+100*c+10*b+a
     [a,b,c,d]
+
+-- True if all elements of a list are differnet
+different :: Ord a => [a] -> Bool
+different l = length l == (size . fromList) l
+
 ```
 
 The complete source is [multiplication.hs](multiplication.hs)
--- multiplication.hs
+++ multiplication.hs
@@ -3,10 +3,6 @@ import Data.Set (size, fromList)
 
 main = putStrLn $ foldl (++) "" (map show solution)
 
--- True if all elements of a list are differnet
-different :: Ord a => [a] -> Bool
-different l = length l == (size . fromList) l
-
 solution :: [Int]
 solution = do
     let digit = [0..9]
@@ -17,3 +13,8 @@ solution = do
     guard $ different [a,b,c,d]
     guard $ (1000*a+100*b+10*c+d)*d == 1000*d+100*c+10*b+a
     [a,b,c,d]
+
+-- True if all elements of a list are differnet
+different :: Ord a => [a] -> Bool
+different l = length l == (size . fromList) l
+
