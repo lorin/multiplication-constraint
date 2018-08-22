969d90f8fb92de23b6384cdf54475ae3374d6647
Lorin Hochstein <lhochstein@netflix.com>
2018-08-21 22:41:42 -0700

    Use set to check all different

--- multiplication.hs
+++ multiplication.hs
@@ -1,6 +1,8 @@
 import Control.Monad
+import Data.Set (size, fromList)
 
 main = putStrLn $ foldl (++) "" (map show solution)
+
 
 solution :: [Int]
 solution = do
@@ -9,10 +11,6 @@ solution = do
     b <- digit
     c <- digit
     d <- digit
-    guard $ a/=b
-    guard $ a/=c
-    guard $ a/=d
-    guard $ b/=c
-    guard $ b/=d
+    guard $ (size . fromList $ [a,b,c,d]) == 4
     guard $ (1000*a+100*b+10*c+d)*d == 1000*d+100*c+10*b+a
     [a,b,c,d]
