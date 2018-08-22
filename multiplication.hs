2c1bfa1a65ffd401d2dd7339f8b0290cbb9d333e
Lorin Hochstein <lhochstein@netflix.com>
2018-08-21 22:46:13 -0700

    use a different method

--- multiplication.hs
+++ multiplication.hs
@@ -3,6 +3,9 @@ import Data.Set (size, fromList)
 
 main = putStrLn $ foldl (++) "" (map show solution)
 
+-- Check if all elements of a list are differnet
+different :: Ord a => [a] -> Bool
+different l = length l == (size . fromList) l
 
 solution :: [Int]
 solution = do
@@ -11,6 +14,6 @@ solution = do
     b <- digit
     c <- digit
     d <- digit
-    guard $ (size . fromList $ [a,b,c,d]) == 4
+    guard $ different [a,b,c,d]
     guard $ (1000*a+100*b+10*c+d)*d == 1000*d+100*c+10*b+a
     [a,b,c,d]
